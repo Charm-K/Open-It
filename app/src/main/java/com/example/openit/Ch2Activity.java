cb104bb946c634e14528ad78039e4f18da941ed2
attowls <planting31@ajou.ac.kr>
2021-08-29 21:10:37 +0900

    modify CH2 and etc

--- app/src/main/java/com/example/openit/Ch2Activity.java
+++ app/src/main/java/com/example/openit/Ch2Activity.java
@@ -2,13 +2,80 @@ package com.example.openit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.media.AudioManager;
+import android.media.SoundPool;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class Ch2Activity extends AppCompatActivity {
+
+    public ImageView c2_button;
+    public ImageView c2_ment;
+    public ImageView c2_human;
+    SoundPool soundPool;
+    int soundID;
+    Animation animTransRight;
+    public boolean check_on = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ch2);
+
+        c2_button = (ImageView)findViewById(R.id.ch2_button);
+        c2_ment = (ImageView)findViewById(R.id.ch2_ment);
+        c2_human = (ImageView)findViewById(R.id.ch2_player);
+        soundPool = new SoundPool(1,AudioManager.STREAM_MUSIC, 0);
+        soundID = soundPool.load(this,R.raw.siren_sound,0);
+        animTransRight = AnimationUtils.loadAnimation(this,R.anim.anim_translate_right);
+    }
+
+    public void pushBell(View v){
+        if(check_on==false){
+            check_on = true;
+            fadeInAndHideImage(c2_ment);
+            soundPool.play(soundID,1f,1f,0,0,1.1f);
+
+            Handler handler1 = new Handler();
+            handler1.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    fadeInAndHideImage(c2_human);
+                    c2_human.startAnimation(animTransRight);
+                }
+            }, 2000);
+
+            Intent intent = new Intent(this, Ch3Activity.class);
+            Handler handler = new Handler();
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    startActivity(intent);
+                    overridePendingTransition(R.anim.fadein, R.anim.fadeout);
+                    finish();
+                }
+            }, 5000);
+        }
+
+    }
+
+    private void fadeInAndHideImage(final ImageView img) { //이미지뷰 페이드인
+        Animation fadeIn = new AlphaAnimation(0, 1);
+        fadeIn.setInterpolator(new AccelerateInterpolator());
+        fadeIn.setDuration(500);
+        fadeIn.setAnimationListener(new Animation.AnimationListener() {
+            public void onAnimationEnd(Animation animation) {
+                img.setVisibility(View.VISIBLE); }
+            public void onAnimationRepeat(Animation animation) {}
+            public void onAnimationStart(Animation animation) {}
+        });
+        img.startAnimation(fadeIn);
     }
 }
--- app/src/main/java/com/example/openit/Stubborn.java
+++ app/src/main/java/com/example/openit/Stubborn.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.media.Image;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.animation.AccelerateInterpolator;
 import android.view.animation.AlphaAnimation;
@@ -17,6 +18,8 @@ public class Stubborn extends AppCompatActivity {
     ImageView st_box1;
     ImageView st_box2;
 
+    ImageView st_set1;
+    ImageView st_set2;
     ImageView st_set;
 
     ImageView st_sup;
@@ -33,27 +36,77 @@ public class Stubborn extends AppCompatActivity {
         st_box1 = (ImageView)findViewById(R.id.stub_box1);
         st_box2 = (ImageView)findViewById(R.id.stub_box2);
         st_set = (ImageView)findViewById(R.id.stub_set1);
+        st_set1 = (ImageView)findViewById(R.id.stub_set1);
+        st_set2 = (ImageView)findViewById(R.id.stub_set2);
         st_sup = (ImageView)findViewById(R.id.stub_support1);
+        st_set1.setOnTouchListener(this::dragSet);
     }
 
+    /*
+    STEP1) 완강기 상자 열기
+     */
     public void openBox(View view){
         if(st_step == 1){
             st_box1.setVisibility(View.INVISIBLE);
             st_box2.setVisibility(View.VISIBLE);
-            fadeInAndHideImage(st_set);
+            fadeInAndHideImage(st_set1);
             st_step++;
         }
     }
 
-    public void set(View view){
-        if(st_step == 2){
+    /*
+    STEP2) 완강기 고리에 걸기
+     */
+    public boolean dragSet(View v, MotionEvent event) {
+        float posX = st_set2.getX();
+        float posY = st_set2.getY();
+        float eventX = event.getRawX();
+        float eventY = event.getRawY();
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                return true;
+            case MotionEvent.ACTION_MOVE:
+                st_set1.setX(eventX - st_set1.getWidth()/2); st_set1.setY(eventY - st_set1.getHeight()/2);
+                float setX = st_set1.getX() + st_set1.getWidth()/2;
+                float setY = st_set1.getY() + st_set1.getHeight()/2;
+                if (st_step==2
+                        &&setX >= posX && setY >= posY
+                        && setX <= posX + st_set2.getWidth() && setY <= posY + st_set2.getHeight()/8) {
+                    st_set1.setVisibility(View.INVISIBLE);
+                    st_set = (ImageView)findViewById(R.id.stub_set2);
+                    fadeInAndHideImage(st_set);
+                    st_step++;
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+                break;
+            default:
+                return false;
+        }
+        return true;
+    }
+
+    /*
+    STEP3) 지지대 바깥쪽으로 열기
+     */
+    public void openSupport(View view){
+        if(st_step==3){
             st_set.setVisibility(View.INVISIBLE);
-            st_set = (ImageView)findViewById(R.id.stub_set2);
+            st_set = (ImageView)findViewById(R.id.stub_set3);
             fadeInAndHideImage(st_set);
+            st_sup.setVisibility(View.INVISIBLE);
+            st_sup = (ImageView)findViewById(R.id.stub_support2);
+            fadeInAndHideImage(st_sup);
             st_step++;
         }
+    }
 
-        else if(st_step == 4){
+    /*
+    STEP4) 릴을 창밖으로 던지기
+     */
+    public void set(View view){
+        if(st_step == 4){
             st_set.setVisibility(View.INVISIBLE);
             st_set = (ImageView)findViewById(R.id.stub_set4);
             st_set.setVisibility(View.VISIBLE);
@@ -64,6 +117,10 @@ public class Stubborn extends AppCompatActivity {
 
     }
 
+    /*
+    STEP5) 가슴밸트 장착하기
+    STEP6) 창밖으로 나가기
+     */
     public void player(View view){
         if(st_step == 5){
             st_set.setVisibility(View.INVISIBLE);
@@ -81,29 +138,19 @@ public class Stubborn extends AppCompatActivity {
             handler.postDelayed(new Runnable() {
                 @Override
                 public void run() {
-                    goFinal();
+                    goEscape();
                 }
             }, 1000);
 
         }
     }
 
-    public void openSupport(View view){
-        if(st_step==3){
-            st_set.setVisibility(View.INVISIBLE);
-            st_set = (ImageView)findViewById(R.id.stub_set3);
-            fadeInAndHideImage(st_set);
-            st_sup.setVisibility(View.INVISIBLE);
-            st_sup = (ImageView)findViewById(R.id.stub_support2);
-            fadeInAndHideImage(st_sup);
-            st_step++;
-        }
-    }
 
-    public void goFinal(){
-        Intent intent = new Intent(this, Final.class);
+    public void goEscape(){
+        Intent intent = new Intent(this, Escape.class);
         startActivity(intent);
         overridePendingTransition(R.anim.fadein, R.anim.fadeout);
+        st_step = 1;
     }
 
     private void fadeInAndHideImage(final ImageView img) { //이미지뷰 페이드인
